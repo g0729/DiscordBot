712b9d87658bb1d1288e6fedd2a1979cddaff23c
g0729 <151703074+g0729@users.noreply.github.com>
2024-12-02 00:25:29 +0900

    .

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ ytdl_format_options = {
     "no_warnings": True,
     "default_search": "auto",
     "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    "cookiefile": "cookies.txt",  # 쿠키 파일 경로 설정
 }
 
 ffmpeg_options = {
@@ -142,22 +143,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
-        ytdl_opts = {
-            "format": "bestaudio/best",
-            "quiet": True,
-            "default_search": "auto",
-            "noplaylist": True,
-            "cookiefile": "cookies.txt",  # 쿠키 파일 경로 설정
-        }
-        ytdl = youtube_dl.YoutubeDL(ytdl_opts)
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
         if "entries" in data:
-            data = data["entries"][0]  # 재생목록의 첫 번째 항목 가져오기
+            # take first item from a playlist
+            data = data["entries"][0]
 
         filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename), data=data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 music_queue = []
